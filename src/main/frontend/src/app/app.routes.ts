094fffb64db8b2e01f0f948b3ff9a365cf36d8d5
yanne duarte <yanne.s.duarte.ext@igfej.mj.pt>
2025-03-28 15:15:08 +0000

    fix: tenhcila aceder a utilizadores

--- src/main/frontend/src/app/app.routes.ts
+++ src/main/frontend/src/app/app.routes.ts
@@ -46,8 +46,16 @@ export const routes: Routes = [
             component: UserFormComponent,
             data: { roles: ['ADMIN'] },
           },
-          { path: ':id', component: UserViewComponent },
-          { path: ':id/edit', component: UserFormComponent },
+          { 
+            path: ':id', 
+            component: UserViewComponent,
+            data: { roles: ['ADMIN', 'TECHNICAL'] }
+          },
+          { 
+            path: ':id/edit', 
+            component: UserFormComponent,
+            data: { roles: ['ADMIN', 'TECHNICAL'] }
+          },
         ],
       },
       {
